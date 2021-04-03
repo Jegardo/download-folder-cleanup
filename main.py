51bc5c99d479b740d7fe0f88df403827a6a94492
Jegardo <45519275+Jegardo@users.noreply.github.com>
2021-04-03 22:18:07 +0300

    make date dir

--- main.py
+++ main.py
@@ -9,16 +9,28 @@ dest_imgs = "C:/Users/pkavv/OneDrive/Εικόνες"
 dest_docs = "C:/Users/pkavv/OneDrive/Έγγραφα"
 dest_zips = "C:/Users/pkavv/OneDrive/Έγγραφα/zips-rars-etc"
 
+
+def makefolder(folder):
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+
+
 for x in os.listdir(path):
+    fname = pathlib.Path('{}/{}'.format(path, x))
+    ctime = datetime.datetime.fromtimestamp(fname.stat().st_ctime)
+
     if re.search('.jpg$', x):
-        fname = pathlib.Path('{}/{}'.format(path, x))
-        ctime = datetime.datetime.fromtimestamp(fname.stat().st_ctime)
-        shutil.move(os.path.join(path, x), os.path.join(dest_docs, '{}-{}.jpg'.format(str(ctime)[:10], x)))
+        directory_img = '{}/{}'.format(dest_imgs, str(ctime)[:7])
+        makefolder(directory_img)
+
+        shutil.move(os.path.join(path, x), os.path.join(directory_img, '{}-{}'.format(str(ctime)[:10], x)))
     elif re.search('.pdf$', x):
-        fname = pathlib.Path('{}/{}'.format(path, x))
-        ctime = datetime.datetime.fromtimestamp(fname.stat().st_ctime)
-        shutil.move(os.path.join(path, x), os.path.join(dest_docs, '{}-{}.pdf'.format(str(ctime)[:10], x)))
+        directory_docs = '{}/{}'.format(dest_docs, str(ctime)[:7])
+        makefolder(directory_docs)
+
+        shutil.move(os.path.join(path, x), os.path.join(directory_docs, '{}-{}'.format(str(ctime)[:10], x)))
     elif re.search('.zip$', x) or re.search('.rar$', x):
-        fname = pathlib.Path('{}/{}'.format(path, x))
-        ctime = datetime.datetime.fromtimestamp(fname.stat().st_ctime)
-        shutil.move(os.path.join(path, x), os.path.join(dest_zips, '{}-{}'.format(str(ctime)[:10], x)))
+        directory_zip = '{}/{}'.format(dest_zips, str(ctime)[:7])
+        makefolder(directory_zip)
+
+        shutil.move(os.path.join(path, x), os.path.join(directory_zip, '{}-{}'.format(str(ctime)[:10], x)))
